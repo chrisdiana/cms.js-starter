5b66591d0998c1d765d439a1ed17cd784d0f42ec
Haruki Takahashi <HarukiTakahashi@users.noreply.github.com>
2022-06-05 06:01:11 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -12,7 +12,7 @@ var config = {
   // and branch to get files from.
   github: {
     username: 'HarukiTakahashi',
-    repo: 'cms.js-starter',
+    repo: 'blog',
     branch: 'gh-pages',
     host: 'https://api.github.com',
     // Use prefix option if your site is located in a subdirectory.
